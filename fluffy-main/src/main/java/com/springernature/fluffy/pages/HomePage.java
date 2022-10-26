0350fb8398e43fbc62c1f572527633d792b34bda
bhanuprakash437 <bhanuprakash437@gmail.com>
2022-10-26 12:37:07 +0200

    revert changes

--- fluffy-main/src/main/java/com/springernature/fluffy/pages/HomePage.java
+++ fluffy-main/src/main/java/com/springernature/fluffy/pages/HomePage.java
@@ -28,10 +28,10 @@ public class HomePage extends AbstractBasePage
     @FindBy(how = How.CSS, using = "#advanced-search-link")
     private WebElement advancedSearchLink;
 
-    @FindBy(how = How.CSS, using = ".cross-nav--wide > .lang span")
+    @FindBy(how = How.CSS, using = ".lang > button > div")
     private WebElement currentLanguage;
 
-    @FindBy(how = How.CSS, using = ".cross-nav--wide .change-language-Deutsch")
+    @FindBy(how = How.CSS, using = ".change-language-Deutsch")
     private WebElement changeLanguageDeutsch;
 
     public HomePage(WebDriver driver)
